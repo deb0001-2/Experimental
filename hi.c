3b7018018332413e6eeed93f51c83063d48aa2e7
Debajyoti Bera <debajyotibera2000@gmail.com>
2026-01-16 23:07:08 +0530

    Update hi.c

--- hi.c
+++ hi.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 
-// Method 1: Using a Function to find GCF
-int gcf(int a, int b) {
-    int result = 1;
-    for (int i = 1; i <= a && i <= b; i++) {
-        if (a % i == 0 && b % i == 0) {
-            result = i;
-        }
-    }
-    return result;
-}
-
 int main() {
     int n1 = 12, n2 = 18;
     int gcd_loop = 1;
-    
-    // --- Approach 1: Using the Function ---
-    printf("Calculated using Function: %d\n", gcf(n1, n2));
-
-    // --- Approach 2: Using a Normal Loop (Reverse Iteration) ---
+    // --- Using a Normal Loop (Reverse Iteration) ---
     // Start from the smaller number and go down.
     // The first common factor we find is GUARANTEED to be the Greatest.
     int min = (n1 < n2) ? n1 : n2;
@@ -28,8 +13,7 @@ int main() {
             break; // Found the highest one, no need to check further!
         }
     }
-
     printf("Calculated using Normal Loop: %d\n", gcd_loop);
-
     return 0;
 }
+
